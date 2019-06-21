e58473bfa033f1ed6096c2588e7ef4ec84b714fe
elangreen <green.elan5@gmail.com>
2019-06-21 08:46:17 -0700

    initial commit

---
+++ src/dogs-router/dogsRouter.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const jsonParser = express.json()
+const dogsRouter = express.Router()
+
+dogsRouter
+  .route('/')
+  .get((req, res, next) => {
+    res.status(200).json({
+      imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+      imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+      name: 'Fluffy',
+      sex: 'Female',
+      age: 2,
+      breed: 'Bengal',
+      story: 'Thrown on the street'
+    })
+  })
+
+module.exports = dogsRouter

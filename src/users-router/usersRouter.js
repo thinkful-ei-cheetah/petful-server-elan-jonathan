08ca8a97cbda39526b375af7d72bc5239e1218a3
Jonathan Lassen <jonathan.lassen@gmail.com>
2019-06-21 17:45:42 -0400

    fixed dequeing users

--- src/users-router/usersRouter.js
+++ src/users-router/usersRouter.js
@@ -18,10 +18,15 @@ usersRouter
       while (curr_node)
       {
         //console.log(Date.now() > curr_node.data.entertime+60000)
-        if (Date.now() > curr_node.data.entertime+(60000*5))
+        if (Date.now() > curr_node.data.entertime+(3000*5))
         {
           if (curr_node.next)
+          {
             userQueue.first = curr_node.next;
+          } else
+          {
+            userQueue.first =null;
+          }
         } else
         {
           counter++;
